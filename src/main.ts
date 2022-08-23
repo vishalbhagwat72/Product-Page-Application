4b2bf7177daa95528e85180d6043e3f53b496c0c
vishalbhagwat72 <vishalbhagwat72@gmail.com>
2022-08-23 09:57:32 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+enableProdMode();
+
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
